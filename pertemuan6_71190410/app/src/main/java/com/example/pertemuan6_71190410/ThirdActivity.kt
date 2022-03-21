84bce8902e6bfbe0d8c403c1ba37b5b3aa2efe38
bastiansurya <bastiansurya777@gmail.com>
2022-03-21 14:55:00 +0700

    Pertemuan 6
    Fragment

---
+++ pertemuan6_71190410/app/src/main/java/com/example/pertemuan6_71190410/ThirdActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pertemuan6_71190410
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ThirdActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity3)
+    }
+}

bc1cae685d4eca87934f8ee8173e71a2dada4684
bastiansurya <bastiansurya777@gmail.com>
2022-04-25 14:21:36 +0700

    Pertemuan 8
    Toolbar

---
+++ pertemuan8_71190410/app/src/main/java/com/example/pertemuan8_71190410/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.pertemuan8_71190410
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar1))
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_tool, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val in1 = Intent(this, FavoriteActivity::class.java)
+        val in2 = Intent(this, PeopleActivity::class.java)
+        val in3 = Intent(this, MainActivity::class.java)
+        when(item.itemId){
+            R.id.favorite -> startActivity(in1)
+            R.id.peoples -> startActivity(in2)
+            R.id.home -> startActivity(in3)
+        }
+
+        return true
+    }
+}
---
+++ pertemuan8_71190410/app/src/main/java/com/example/pertemuan8_71190410/PeopleActivity.kt
@@ -0,0 +1,34 @@
+package com.example.pertemuan8_71190410
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
+class PeopleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_people)
+
+        setSupportActionBar(findViewById(R.id.toolbar1))
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_tool, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val in1 = Intent(this, FavoriteActivity::class.java)
+        val in2 = Intent(this, PeopleActivity::class.java)
+        val in3 = Intent(this, MainActivity::class.java)
+        when(item.itemId){
+            R.id.favorite -> startActivity(in1)
+            R.id.peoples -> startActivity(in2)
+            R.id.home -> startActivity(in3)
+        }
+
+        return true
+    }
+}
